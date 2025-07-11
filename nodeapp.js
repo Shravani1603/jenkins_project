651b6bbc7a0fe7be88f0c993298126b64dce8d2b
Shravani1603 <149895354+Shravani1603@users.noreply.github.com>
2025-07-11 13:01:15 +0530

    Update nodeapp.js

--- nodeapp.js
+++ nodeapp.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app-get("/", (req, res) => {
-res-send("Node-js app deployed to AWS Elastic Beanstalkl & lley i am shravani.");
+app.get("/", (req, res) => {
+  res.send("Node.js app deployed to AWS Elastic Beanstalk! Hey, I am Shravani.");
 });
-app-listen(port, () => {
-console.log("App running on port ${port}");
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
